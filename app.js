af05782751f9f7ba4f186ab232ebac73e283304d
Bangxin Hu <hu.bangxin@gmail.com>
2020-12-12 18:19:54 -0500

    Add script

---
+++ app.js
@@ -0,0 +1,57 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// Get Quote From API
+async function getQuote() {
+    loading();
+    const url = 'http://quotes.rest/qod.json?category=inspire';
+
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        quoteText.innerText = data.contents.quotes[0].quote;
+        if (quoteText.innerText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        authorText.innerText = data.contents.quotes[0].author || 'Unknown';
+        complete();
+    } catch (error) {
+        console.log('No quote can be fetched.', error)
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+// On Load
+getQuote();
